4583d8e36012fbc18ea4f9c018d1c860f2d7b622
rakesh4stepsdigital <rakesh@4stepsdigital.com>
2025-05-26 17:43:38 +0530

    add login popup in car pages

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -4,9 +4,10 @@ import Footer from "@/components/Footer";
 import { faStar, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import Login from "@/components/Login";
 
 export default function Cart() {
-  const { cartItems, removeFromCart, incrementQuantity, decrementQuantity } =
+  const { cartItems, removeFromCart, incrementQuantity, decrementQuantity, loginPopup } =
     useCart();
 
   const total = cartItems.reduce(
@@ -16,6 +17,7 @@ export default function Cart() {
   console.log("cartItems", cartItems);
   return (
     <>
+    {loginPopup && <Login />}
       <Header />
       <div className="pt-[55px]">
       <div className="container mx-auto py-10">
@@ -128,64 +130,3 @@ export default function Cart() {
     </>
   );
 }
-
-// import Header from '@/components/Header';
-// import { useCart } from '../context/CartContext';
-// import Footer from '@/components/Footer';
-
-// export default function Cart() {
-//   const { cartItems, removeFromCart, incrementQuantity, decrementQuantity } = useCart();
-
-//   const total = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
-//   console.log("cartItems",cartItems);
-//   return (
-//     <>
-//       <Header />
-//       <div className="p-4">
-//         <h1 className="text-xl font-bold mb-4">Cart</h1>
-
-//         {cartItems.length === 0 ? (
-//           <p>Your cart is empty.</p>
-//         ) : (
-//           cartItems.map((item) => (
-//             <div key={item.id} className="mb-4 border p-4 rounded">
-//               <h2 className="text-lg font-bold">{item.title}</h2>
-//               <p>Price: ${item.price}</p>
-
-//               {/* Quantity Controls */}
-//               <div className="flex items-center gap-2 my-2">
-//                 <button
-//                   onClick={() => decrementQuantity(item.id)}
-//                   className="bg-gray-200 px-2 py-1 rounded"
-//                 >
-//                   -
-//                 </button>
-//                 <span className="font-medium">{item.quantity}</span>
-//                 <button
-//                   onClick={() => incrementQuantity(item.id)}
-//                   className="bg-gray-200 px-2 py-1 rounded"
-//                 >
-//                   +
-//                 </button>
-//               </div>
-
-//               <p>Subtotal: ${item.price * item.quantity}</p>
-
-//               <button
-//                 onClick={() => removeFromCart(item.id)}
-//                 className="mt-2 bg-red-500 text-white px-3 py-1 rounded"
-//               >
-//                 Remove
-//               </button>
-//             </div>
-//           ))
-//         )}
-
-//         {cartItems.length > 0 && (
-//           <h2 className="text-xl font-bold mt-6">Total: ${total.toFixed(2)}</h2>
-//         )}
-//       </div>
-//       <Footer />
-//     </>
-//   );
-// }
